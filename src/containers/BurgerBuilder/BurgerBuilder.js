e4e658bd72599f1f355901efcd5790cee5d53002
ProUnebit <ProUnebit@yandex.ru>
2018-08-24 03:38:48 +0300

    add calculating the ingridients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,26 @@ import Input from '../../hoc/Input'
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            ingredients: {
+                salad: 1,
+                bacon: 1,
+                cheese: 2,
+                meat: 2
+            }
+        }
+    }
+
     render () {
+
+        let { ingredients } = this.state;
+
         return (
             <Input>
-                <Burger />
+                <Burger ingredients = {ingredients}/>
                 <div>Build Controls</div>
             </Input>
         )
