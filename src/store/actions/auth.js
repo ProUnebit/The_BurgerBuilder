e2d140dfbdc770ed93f041ae85d3b5297a5a0264
ProUnebit <ProUnebit@yandex.ru>
2018-09-14 19:06:24 +0300

    added checkAuthTimeout logout time in actions(auth.js)

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,6 +22,18 @@ export const authFail = (error) => {
     }
 }
 
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    }
+}
+
+export const checkAuthTimeout = (expirationTime) => (dispatch) => {
+    setTimeout(() => {
+        dispatch(logout());
+    }, expirationTime * 1000);
+}
+
 export const auth = (email, password, isSignup) => (dispatch) => {
     dispatch(authStart());
     const authData = {
@@ -36,6 +48,7 @@ export const auth = (email, password, isSignup) => (dispatch) => {
     axios.post(url, authData)
         .then(res => {
             dispatch(authSuccess(res.data.idToken, res.data.localId));
+            dispatch(checkAuthTimeout(res.data.expiresIn))
         })
         .catch(err => {
             dispatch(authFail(err.response.data.error));
